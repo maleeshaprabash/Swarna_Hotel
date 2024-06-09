3f454d0c49df56b1a6f8c11a1ff199cbdc8e8129
maleeshaprabash <162019809+maleeshaprabash@users.noreply.github.com>
2024-06-09 21:53:25 +0530

    Signup.java

--- Signup.java
+++ Signup.java
@@ -263,9 +263,7 @@ public class SignUp extends javax.swing.JFrame {
         this.dispose();
     }                                        
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
